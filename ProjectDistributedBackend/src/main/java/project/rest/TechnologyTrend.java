983079f61f6f29d3cffd650ddf881754f09d30f1
Ivan Lapickij <ivan.lapickij@gmail.com>
2025-03-27 21:53:14 +0000

    Initial commit

---
+++ ProjectDistributedBackend/src/main/java/project/rest/TechnologyTrend.java
@@ -0,0 +1,28 @@
+package project.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/techno")
+public class TechnologyTrend {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello from Ivan";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" +"<hello> Hello Jersey XML" + "</hello>";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+				+ "<body><h1>" + "Hello Jersey HTML" + "</body></h1>" + "<html>";
+	}
+}
